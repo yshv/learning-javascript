8687360ae1ffb23188a2e20503c268ec8196bd6b
amanvir <yashvir.sangha@gmail.com>
2015-04-19 20:44:02 +0100

    add first ever game

---
+++ first_game.js
@@ -0,0 +1,6 @@
+var adjective = prompt("Pick an adjective");
+var noun = prompt("Now pick a noun");
+var verb = prompt("And finally pick a verb");
+alert("Done, are you ready for your story?");
+var message = " Once apon a time there was a " + adjective +" "+ noun + " who planned to " + verb + " the entire universe! ";
+document.write(message);
